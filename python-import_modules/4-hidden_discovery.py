3bfe1cf33070133a3cd8035b77a80a11db7eeb58
kmehemmed <kelbiyevmehemmed90@gmail.com>
2026-04-04 18:44:02 +0000

    task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,6 @@ import hidden_4
 
 if __name__ == "__main__":
     names = dir(hidden_4)
-    names.sort()
     for name in names:
-                if not name.startswith("__"):
+        if not name.startswith("__"):
             print(name)
