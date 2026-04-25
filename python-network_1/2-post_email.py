dd0ed09de466fbd4df88052b8ecba9b904ac5fdb
kmehemmed <kelbiyevmehemmed90@gmail.com>
2026-04-25 12:27:50 +0000

    task2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email, sends a POST request to the passed URL"""
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('ascii')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
